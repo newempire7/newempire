8fe141f9ee9a7e622213b35d4c50237550279106
DoIn Sin <121351760+newdoin@users.noreply.github.com>
2023-10-24 05:48:31 +0900

    Update ProfileCard.tsx

--- src/routes/Feed/ProfileCard.tsx
+++ src/routes/Feed/ProfileCard.tsx
@@ -72,14 +72,12 @@ const StyledWrapper = styled.div`
         color: ${({ theme }) => theme.colors.gray11};
       }
       .bio {
-        width: 150px; 
-        text-align:center;
         margin-bottom: 0.5rem;
         font-size: 0.875rem;
         line-height: 1.25rem;
       }
       .text-sm mb-2 {
-         width: 150px; 
+        width: 150px; 
         text-align:center;
     }
   }
